7324a950bf5d361c4555160bd466e3bc87946cb4
Phil Gengler <phil@pgengler.net>
2014-10-09 23:17:36 -0400

    Have Unicorn listen on a UNIX socket instead of a TCP port

--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -4,6 +4,6 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen 5002
+listen "/tmp/ip-tracker.unicorn.sock"
 worker_processes 2
 timeout 30
